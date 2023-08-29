6ccfd7c7ee4bd35d19b8f740d53d81f4cab6cf56
pan-ti <pandeandromeda@gmail.com>
2023-08-28 23:23:48 -0300

    declaração de função chamada ordenarPropriedades. Essa função será responsável por ordenar e mostrar as propriedades de CSS que foram coletadas anteriormente.Método .sort sendo chamado no array propriedades para reorganizar os elementos em ordem alfabética.O resultado é armazenado na variável propriedadesOrdenadas.Criação de um loop for ... of para percorrer cada elemento do array  e console.log  em propriedade para mostrar uma em cada linha ordenadas

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ import prompt from 'prompt-sync';
 
 const propriedades = [];
 const promptSync = prompt();
+
+function ordenarPropriedades() {
+    const propriedadesOrdenadas = propriedades.sort();
+    for (const propriedade of propriedadesOrdenadas) {
+        console.log(propriedade);
+    }
+}
